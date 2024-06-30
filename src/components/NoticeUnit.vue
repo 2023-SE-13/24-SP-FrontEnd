4c58bd5fdee556694100bc1bd478a7cf253cc0b6
suin14 <sil0410@163.com>
2024-06-30 17:07:10 +0800

    重新设计了通知样式

--- src/components/NoticeUnit.vue
+++ src/components/NoticeUnit.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="notice">
-    <p style="top:19%;left: 35%">尊敬的{{ letterData.username }}：</p>
-    <p style="top:29%;left: 35%">您好！</p>
-    <p style="top:33.5%;left: 35%">我们非常高兴地通知您，经过公司严格的评估，</p>
-    <p style="top:38%;left: 35%">您已被录用为我司{{ letterData.position_name }}职位。恭喜您！</p>
-    <p style="top:46.5%;left: 35%">我们期待您的加入，并相信您的才能和经验将</p>
-    <p style="top:51.2%;left: 35%">为公司的发展注入新的活力。如果您有任何疑</p>
-    <p style="top:55.5%;left: 35%">问或需要进一步的信息，请随时与我们联系。</p>
-    <p style="top:64%;left: 35%">再次祝贺您！</p>
-    <p style="top:73.5%;left: 35%">{{ letterData.company_name }}</p>
-    <p style="top:78%;left: 35%">{{ letterData.date }}</p>
+    <p style="top:20.5%;left: 15%">尊敬的{{ letterData.username }}：</p>
+    <p style="top:25%;left: 15%">您好！</p>
+    <p style="top:33.5%;left: 15%">我们非常高兴地通知您，经过公司严格的评估，您已被录用为我司{{ letterData.position_name }}职位。</p>
+    <p style="top:38%;left: 15%">恭喜您！</p>
+    <p style="top:46.5%;left: 15%">我们期待您的加入，并相信您的才能和经验将为公司的发展注入新的活力。</p>
+    <p style="top:51.2%;left: 15%">如果您有任何疑问或需要进一步的信息，请随时与我们联系。</p>
+    <p style="top:59.5%;left: 15%">再次祝贺您！</p>
+    <p style="top:69%;left: 15%">{{ letterData.company_name }}</p>
+    <p style="top:73%;left: 15%">人力资源部</p>
+    <p style="top:78%;left: 15%">{{ letterData.date }}</p>
 
     <div class="custom-btn">
-      <button style="top: 78%; right: 25%" @click="accept">接受</button>
-      <button style="top: 78%; right: 17%" @click="refuse">拒绝</button>
+      <button style="top: 82%; right: 50%" @click="accept">接受</button>
+      <button style="top: 82%; right: 42%" @click="refuse">拒绝</button>
     </div>
   </div>
 </template>
@@ -56,6 +56,10 @@ export default {
   position: absolute;
   font-size: 1.1em;
   border-radius: 5px;
+}
+
+.custom-btn button:hover {
+  background-color: #00d1d0;
 }
 
 .notice {
