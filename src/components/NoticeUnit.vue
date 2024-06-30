bca3b058503e6c0536bcb76b66a84fa0b5a37ff6
suin14 <sil0410@163.com>
2024-06-30 16:14:10 +0800

    招聘通知样式

--- src/components/NoticeUnit.vue
+++ src/components/NoticeUnit.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="notice">
-    <p style="top:19%;left: 32%">尊敬的[应聘者姓名]：</p>
+    <p style="top:19%;left: 32%">尊敬的{{ letterData.username }}：</p>
     <p style="top:28.5%;left: 32%">您好！</p>
     <p style="top:33%;left: 32%">我们非常高兴地通知您，经过公司严格的评估，</p>
-    <p style="top:33%;left: 32%">您已被录用为我司[职位名称]职位。恭喜您！</p>
-    <p style="top:48.5%;left: 32%">我们期待您的加入，并相信您的才能和经验将为公司的发展注入新的活力。如果您有任何疑问或需要进一步的信息，请随时与我们联系。</p>
-    <p style="top:58.5%;left: 32%">再次祝贺您！</p>
-    <p style="top:68.5%;left: 32%">此致</p>
-    <p style="top:78.5%;left: 32%">敬礼</p>
-    <p style="top:88.5%;left: 32%">[公司名称]</p>
-    <p style="top:98.5%;left: 32%">人力资源部</p>
-    <p style="top:108.5%;left: 32%">[日期]</p>
+    <p style="top:37.5%;left: 32%">您已被录用为我司{{ letterData.position_name }}职位。恭喜您！</p>
+    <p style="top:46.5%;left: 32%">我们期待您的加入，并相信您的才能和经验将</p>
+    <p style="top:51.5%;left: 32%">为公司的发展注入新的活力。如果您有任何疑</p>
+    <p style="top:56%;left: 32%">问或需要进一步的信息，请随时与我们联系。</p>
+    <p style="top:65%;left: 32%">再次祝贺您！</p>
+    <p style="top:74%;left: 32%">{{ letterData.company_name }}</p>
+    <p style="top:78.5%;left: 32%">{{ letterData.date }}</p>
+
+    <button>接受</button>
+    
   </div>
 </template>
 
@@ -18,7 +20,12 @@
 export default {
   data() {
     return {
-
+      letterData:{
+        username:"xxx",
+        position_name: "xxx",
+        company_name: "XXX",
+        date: "2024-00-00"
+      }
     }
   },
   props: {
